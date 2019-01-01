638375a83f2f020bddbe7cf1f9a66f5b0beb1118
Kevin <36828226+neo250376@users.noreply.github.com>
2019-01-01 13:16:59 +0000

    Update Bogdan_auto18.04.sh

--- Bogdan_auto18.04.sh
+++ Bogdan_auto18.04.sh
@@ -65,7 +65,7 @@ echo '#! /bin/bash
 # adjust -b & -t value as described in the README and FAQ
 worker="OvErLoDe"
 pool="http://arionum.rocks"
-wallet="65AkkjBs2arwbikYVDh3B57aeehzpVp9Xw69tgewj8y8stx9FjajNhxR5Y3D9vzjYGgPGzuXbf7xSKn1C2i2DxFY"
+wallet="WALLET_ADDRESS"
 cpuintensity="100"
 gpuintensitycblocks="0"
 gpuintensitygblocks="61"
